6d324b899b31bf024633f59954671e7f0cecda91
Josh Peters <jpeters@wistia.com>
2023-03-31 15:21:49 -0400

    added a test

--- r001z/spec/r001z_spec.rb
+++ r001z/spec/r001z_spec.rb
@@ -5,7 +5,12 @@ RSpec.describe R001z do
     expect(R001z::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "test app returns success" do
+    env = {
+      "PATH_INFO" => "/",
+      "QUERY_STRING" => "",
+    }
+
+    expect(200).to eq R001z::App.new.call(env).first
   end
 end
